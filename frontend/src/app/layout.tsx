7b00d266b52d966e659bcdf99e9e1cc6903f929c
BeastxD7 <shashankdevadiga2003@gmail.com>
2025-04-06 00:19:09 +0530

    Configured NextJs

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Swastify | Where Healthcare Meets Innovation.",
+  description: "Swastify is a healthcare platform focused on solving real-world medical challenges. We are building a comprehensive solution that empowers users, doctors, and hospitals to manage healthcare more efficiently, improve patient outcomes, and innovate the future of healthcare.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
